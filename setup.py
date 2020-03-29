4fe208485d7fc7ae8a3456043d9ae6c6bb6a981c
Miguel G. Flores <miguelgzflores@gmail.com>
2020-03-29 19:44:27 +0200

    Github action to publish to Pypi

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+with open('README.md', 'r') as readme:
+    README = readme.read()
+
+setup(
+    name='Flask-Pypendency',
+    version='0.1b',
+    url='https://github.com/miguelgf/pypendency-flask',
+    license='MIT License',
+    author='Miguel González Flores',
+    author_email='miguelgzflores@gmail.com',
+    description='Flask extension for Pypendency, a dependency injection tool',
+    long_description=README,
+    packages=['flask_pypendency'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Pypendency'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Typing :: Typed',
+    ],
+    python_requires='>=3.6',
+)
