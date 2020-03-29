9e686bc6a00b0241e6ba5b13ca1f2b565d0460e7
Miguel G. Flores <miguelgzflores@gmail.com>
2020-03-29 20:24:15 +0200

    Add full flask extension integration test

---
+++ test/test_flask_pypendency.py
@@ -0,0 +1,27 @@
+import os
+from unittest import TestCase
+
+from flask import Flask
+
+from flask_pypendency import Pypendency
+from test.resources.test_full.autodiscover1.a import A
+from test.resources.test_full.autodiscover2.b import B
+
+
+class TestFlaskPypendency(TestCase):
+    def test_flask_pypendency(self):
+        app = Flask(__name__)
+        test_folder = os.path.dirname(os.path.abspath(__file__))
+        app.config.from_mapping(
+            PYPENDENCY_DI_FOLDER_NAME="_di",
+            PYPENDENCY_DISCOVER_PATHS=[
+                os.path.join(test_folder, "resources/test_full")
+            ]
+        )
+
+        Pypendency(app)
+
+        self.assertIsInstance(app.container.get("test.resources.test_full.autodiscover1.a.A"), A)
+        self.assertIsInstance(app.container.get("test.resources.test_full.autodiscover2.b.B"), B)
+
+
